b1d9fa384f221d766c0139bccb9748680e21fe43
LaurenTaylor92 <118805680+LaurenTaylor92@users.noreply.github.com>
2023-01-10 11:48:04 +0100

    Changed icon url to SheCodes. Added padding to ul and updated name of class (details), corrected spelling error (submit)

--- src/app.js
+++ src/app.js
@@ -65,6 +65,7 @@ function handleSubmit(event) {
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
+
   celsiusLink.classList.remove("active");
   fahrenheitLink.classList.add("active");
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
@@ -82,7 +83,7 @@ function displayCelsiusTemperature(event) {
 let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
-form.addEventListener("sumbit", handleSubmit);
+form.addEventListener("submit", handleSubmit);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
